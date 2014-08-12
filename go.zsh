22a3176755bba7ff8604f943fc5a98e525e49b2e
Vangelis Tsoumenis <kioopi@gmail.com>
2014-08-12 17:20:19 +0200

    add gopkgs/bin to path

--- go.zsh
+++ go.zsh
@@ -3,7 +3,7 @@
 export GOPATH=/home/vt/gopkgs
 export GOSHARE=/usr/share/go
 
-PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:
+PATH=$PATH:/usr/local/go/bin:/home/vt/gopkgs/bin:
 
 source $GOSHARE/misc/zsh/go
 
